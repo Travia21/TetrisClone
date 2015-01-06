e2995c515753427b4ce6ec5ae0eb35c743136b08
Steven Peterson <travia21@gmail.com>
2015-01-06 05:28:25 -0800

    Got something working.

---
+++ Tetris/src/com/donnellpetersonexperience/networking/ServerManager.java
@@ -0,0 +1,69 @@
+package com.donnellpetersonexperience.networking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Steven on 1/6/15.
+ */
+public class ServerManager {
+    private Set<PrintWriter> connections;
+
+    public void ServerManager() {
+        try{
+            ServerSocket server = new ServerSocket(9898);
+            System.out.println("Server started.");
+            connections = new HashSet<PrintWriter>();
+            while(true){
+                try{
+                    new Connection(server.accept()).start();
+                }catch(IOException e) {
+                }finally{
+                    server.close();
+                }
+            }
+        }catch(IOException e){}
+    }
+
+    private void broadcast(String message){
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm zzz");
+        String user = "<" + sdf.format(date) + "> Steven: ";
+
+        for(PrintWriter connection : connections){
+            connection.println(user + message);
+        }
+    }
+
+    private class Connection extends Thread {
+        private BufferedReader in;
+        private PrintWriter out;
+        private Socket socket;
+
+        public Connection(Socket sock) {
+            socket = sock;
+            try{
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                out = new PrintWriter(socket.getOutputStream(), true);
+                connections.add(out);
+            }catch(IOException e){ }
+        }
+
+        @Override
+        public void run(){
+            while(true){
+                try {
+                    broadcast(in.readLine());
+                }catch(IOException e){ }
+            }
+        }
+    }
+}
---
+++ Tetris/src/com/donnellpetersonexperience/networking/SocketManager.java
@@ -0,0 +1,48 @@
+package com.donnellpetersonexperience.networking;
+
+import com.donnellpetersonexperience.builder.WindowBuilder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * Created by mactop on 1/6/15.
+ */
+public class SocketManager extends Thread {
+    private BufferedReader in;
+    private PrintWriter out;
+    private WindowBuilder window;
+    private Socket socket;
+
+    public SocketManager(Socket sock) {
+        socket = sock;
+        System.out.println("Connection to server established.");
+        window = new WindowBuilder(this);
+        try{
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
+        }catch(IOException e){}
+    }
+
+    @Override
+    public void run(){
+        while(true){
+            try {
+                String input = in.readLine();
+                if (input.equals("q")) break;
+                writeToWindow(input);
+            }catch(IOException e){ }
+        }
+    }
+
+    public void write(String str){
+        out.print(str);
+    }
+
+    public void writeToWindow(String str){
+        window.display(str);
+    }
+}
